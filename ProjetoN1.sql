55bf1c15318206911ab1c041f4f69f18a85c32bf
ltees <135617476+ltees@users.noreply.github.com>
2024-09-28 13:24:13 -0300

    ProjetoN1.sql

--- ProjetoN1.sql
+++ ProjetoN1.sql
@@ -34,7 +34,7 @@ CREATE TABLE Pesquisador (
     id_departamento INT,
     id_projeto INT,
     valor_bolsa DECIMAL(10,2) NOT NULL CHECK (valor_bolsa > 0),
-	FOREIGN KEY (id_endereco) REFERENCES Endereco(id_endereco),
+    FOREIGN KEY (id_endereco) REFERENCES Endereco(id_endereco),
     FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento),
     FOREIGN KEY (id_projeto) REFERENCES Projeto(id_projeto)
 );
@@ -60,19 +60,69 @@ VALUES ('Boa Vista', 'Roraima'),
 -- Tabela Departamento
 INSERT INTO Departamento (nome, filial)
 VALUES ('DIDES', 'Boa Vista'),
-       ('DIMAC', 'Palmas');
+       ('DIMAC', 'Palmas'),
+       ('DIRUR','Porto Alegre'),
+       ('DIMAC','Macapa'),
+       ('DIDES','Belem'),
+       ('DIDES','Palmas'),
+       ('DIDES','Boa Vista'),
+       ('DIRUR','Florianopolis'),
+       ('DIMAC','Porto Velho'),
+       ('DIDES','Rio Branco'),
+       ('DINTE','Cabo Frio'),
+       ('DISOC','Campinas'),
+       ('DISOC','Guarulhos'),
+       ('DISOC','Belo Horizonte'),
+       ('DINTE','Sao Paulo');
 
 -- Tabela Financa
 INSERT INTO Financa (fonte_financa, quantia)
-VALUES ('Governo Federal', 50000.00),
-       ('ONG Internacional', 75000.00);
+VALUES ('BID', 100000.00),
+       ('BNDES', 30000.00),
+       ('BID', 100000.00),
+       ('BNDES', 200000.00),
+       ('BNDES', 50000.00),
+       ('BANERJ', 70000.00),
+       ('BANESPA', 45000.00),
+       ('BANESPA', 85000.00),
+       ('BDMG', 120000.00);
+       
 
 -- Tabela Projeto
 INSERT INTO Projeto (nome, id_financa, custo)
-VALUES ('Acesso a Oportunidades', 1, 10000.00),
-       ('Emprego no Turismo', 2, 15000.00);
+VALUES ('Acesso a Oportunidades', 1, 15000.00),
+       ('Brasil em Desenvolvimento', 2, 19000.00),
+       ('Emprego no Turismo', 3, 35000.00),
+       ('MApa das Organizacoes da Sociedade Civil', 4, 55000.00),
+       ('Observatorio de Gestao do Conhecimento', 5, 20000.00),
+       ('Atlas da Violencia', 6, 45000.00),
+       ('Centro de Pesquisa em Ciencia, Tecnologia e Sociedade', 7, 25000.00),
+       ('Retrato das Desigualdades de Genero e Raca', 8, 32000.00),
+       ('Indice de Vulnerabilidade Social', 9, 60000.00);
+       
+       
 
 -- Tabela Pesquisador
 INSERT INTO Pesquisador (nome, status, id_endereco, id_departamento, id_projeto, valor_bolsa)
 VALUES ('Miguel Silva', 'Ativo', 1, 1, 1, 3100.00),
-       ('Arthur Carvalho', 'Ativo', 2, 2, 2, 2500.00);
+       ('Arthur Carvalho', 'Ativo', 2, 2, 2, 2500.00),
+       ('Helena Sousa', 'Inativo', 3, 3, 3, 4000.00),
+       ('Laura Pereira', 'Ativo', 4, 4, 4, 2500.00),
+       ('Gabriel Luz', 'Ativo', 5, 5, 5, 3200.00),
+       ('Samuel Santos', 'Inativo', 6, 6, 6, 2000.00),
+       ('MAria Oliveira', 'Ativo', 7, 7, 7, 2500.00),
+       ('Alice Rodrigues', 'Ativo', 8, 8, 8, 3100.00),
+       ('Bernardo Alves', 'Ativo', 9, 9, 9, 2000.00),
+       ('Theo Gomes', 'Inativo', 10, 10, 10, 2500.00),
+       ('Ailton Santos', 'Ativo', 11, 11, 11, 3100.00),
+       ('Ana Beatriz', 'Ativo', 12, 12, 12, 2100.00),
+       ('Bruno Gomes', 'Inativo', 13, 13, 13, 3500.00),
+       ('Leticia Rodrigues', 'Ativo', 14, 14, 14, 2500.00),
+       ('Vinicius Silva', 'Ativo', 15, 15, 15, 2800.00);
+       
+       select * from Endereco;
+       select * from Departamento;
+       select * from Financa;
+       select * from Projeto;
+       select * from Pesquisador;
+
